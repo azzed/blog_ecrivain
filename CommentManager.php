3a5535540725a19a5dd0d0ed4af021a540448191
azzedine bouzelmad <bouzelmad.azzedin@gmail.com>
2018-01-15 13:12:32 +0100

    Ajout de la fonction addComment

--- CommentManager.php
+++ CommentManager.php
@@ -5,11 +5,18 @@
  * Date: 11/01/2018
  * Time: 12:08
  */
-
+require 'Connexion.php';
+require __DIR__.'/../Entity/Article.php';
 class CommentManager
 {
-    public function addComment($id,$comment)
+    public function __construct()
     {
-
+        $this->connexion = new Connexion();
+    }
+    public function addComment($postId,$comment,$author)
+    {
+        $comments = $this->connexion->prepare('INSERT INTO comment(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
+        $affectedLines = $comments->execute(array($postId, $author, $comment));
+        return $affectedLines;
     }
 }
