5d28d8848d613b6fff867a95879b91f86190635b
azzedine bouzelmad <bouzelmad.azzedin@gmail.com>
2018-01-15 11:25:35 +0100

    initial

---
+++ CommentManager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: utilisateur
+ * Date: 11/01/2018
+ * Time: 12:08
+ */
+
+class CommentManager
+{
+    public function addComment($id,$comment)
+    {
+
+    }
+}
